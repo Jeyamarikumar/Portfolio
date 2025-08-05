86632f28f414459b1bad94c7445327ef939fabf2
Jeyamarikumar <jeyamarikumar162003@gmail.com>
2025-08-05 11:31:03 +0530

    My Portfolio Website

---
+++ project.js
@@ -0,0 +1,9 @@
+function msg()
+{
+    alert('Your Message Send Successfully😊😊😊😊')
+}
+
+function contact()
+{
+    alert('\n'+'\n'+'        Thank you for contacting me'+'\n'+'        Your Action is received '+'\n'+'        You receive email later')
+}
